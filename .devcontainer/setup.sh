8e91bb5d6be2bdef5fc5d626df44a889706a4a8c
peccu <peccul+github@gmail.com>
2024-02-02 16:50:35 +0000

    update setup script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -12,3 +12,21 @@ sudo tailscale up --authkey $TAILSCALE_AUTHKEY
 
 mkdir -p ~/.ssh
 echo "$SSH_PUBKEY" >> ~/.ssh/authorized_keys
+sudo usermod --password $(openssl passwd -1 "$LINE_NOTIFY") $(whoami)
+
+message="Codespace setup finished.
+
+$(whoami)@$(tailscale status | grep $(hostname) | awk '{print $1}')
+
+ssh config looks like below:
+
+Host cs
+  Hostname $(hostname).yourcode.ts.net
+  User $(whoami)
+  IdentityFile /root/.ssh/keys/id_rsa
+  Port 2222
+
+"
+curl -X POST  https://notify-api.line.me/api/notify \
+     -H 'Authorization: Bearer '$LINE_NOTIFY \
+     -F "message=$message"
